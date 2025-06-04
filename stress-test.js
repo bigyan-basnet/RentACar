fcbfc0399153edb133b62f8d4e6911b56a27c6c4
Bigyan Basnet <89792209+bigyan-basnet@users.noreply.github.com>
2025-06-04 10:23:55 +1000

    Update stress-test.js

--- stress-test.js
+++ stress-test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 10000,           // 10,000 virtual users
+  vus: 1000,           // 10,000 virtual users
   duration: '10m',      // run the test for 10 minutes
 };
 
